5879397df8e9a581435aef6719570f77cea5b94b
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-07-22 08:26:53 -0400

    Create chess.rb

---
+++ chess.rb
@@ -0,0 +1,14 @@
+require 'colorize'
+require_relative 'gameboard'
+require_relative 'player'
+require_relative 'sliding_player'
+require_relative 'stepping_player'
+require_relative 'rook'
+require_relative 'bishop'
+require_relative 'queen'
+require_relative 'king'
+require_relative 'knight'
+require_relative 'pawn'
+require_relative 'game'
+
+Game.new.play
